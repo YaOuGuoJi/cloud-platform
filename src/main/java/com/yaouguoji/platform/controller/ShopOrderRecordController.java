c221a9f9be055cd97f385569f560eb3ef1f7a481
Morse1113 <1185585454@qq.com>
2018-09-05 15:55:40 +0800

    add Time parse

--- src/main/java/com/yaouguoji/platform/controller/ShopOrderRecordController.java
+++ src/main/java/com/yaouguoji/platform/controller/ShopOrderRecordController.java
@@ -9,7 +9,6 @@ import com.yaouguoji.platform.enums.HttpStatus;
 import com.yaouguoji.platform.service.OrderRecordService;
 import com.yaouguoji.platform.service.ShopInfoService;
 import com.yaouguoji.platform.vo.ShopOrderRankVO;
-import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.CollectionUtils;
@@ -26,6 +25,7 @@ import java.util.*;
 public class ShopOrderRecordController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ShopOrderRecordController.class);
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     @Resource
     private OrderRecordService orderRecordService;
@@ -33,28 +33,33 @@ public class ShopOrderRecordController {
     private ShopInfoService shopInfoService;
 
     @GetMapping("/order/shop/{shopId}")
-    public CommonResult shopOrder(@PathVariable("shopId") int shopId) {
-        if (shopId <= 0) {
+    public CommonResult shopOrder(@PathVariable("shopId") int shopId, String start, String end) {
+        try {
+            Date startTime = SIMPLE_DATE_FORMAT.parse(start);
+            Date endTime = SIMPLE_DATE_FORMAT.parse(end);
+            if (shopId <= 0 || startTime.after(endTime)) {
+                return CommonResult.fail(HttpStatus.PARAMETER_ERROR);
+            }
+            ShopInfoDTO shopInfoDTO = shopInfoService.findShopInfoByShopId(shopId);
+            if (shopInfoDTO == null) {
+                return CommonResult.fail(HttpStatus.NOT_FOUND);
+            }
+            List<OrderRecordDTO> ordersByShopId = orderRecordService.findOrdersByShopId(shopId, startTime, endTime);
+            Map<String, Object> data = new HashMap<>();
+            data.put("shopInfo", shopInfoDTO);
+            data.put("orderList", ordersByShopId);
+            return CommonResult.success(data);
+        } catch (Exception e) {
+            LOGGER.error("解析时间异常：[{}]", e);
             return CommonResult.fail(HttpStatus.PARAMETER_ERROR);
         }
-        ShopInfoDTO shopInfoDTO = shopInfoService.findShopInfoByShopId(shopId);
-        if (shopInfoDTO == null) {
-            return CommonResult.fail(HttpStatus.NOT_FOUND);
-        }
-        List<OrderRecordDTO> ordersByShopId =
-                orderRecordService.findOrdersByShopId(shopId, new DateTime().minusMonths(1).toDate(), new Date());
-        Map<String, Object> data = new HashMap<>();
-        data.put("shopInfo", shopInfoDTO);
-        data.put("orderList", ordersByShopId);
-        return CommonResult.success(data);
     }
 
     @GetMapping("/order/shop/rank")
     public CommonResult shopOrderRankTop(int limit, String start, String end, int type) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         try {
-            Date startTime = sdf.parse(start);
-            Date endTime = sdf.parse(end);
+            Date startTime = SIMPLE_DATE_FORMAT.parse(start);
+            Date endTime = SIMPLE_DATE_FORMAT.parse(end);
             if (limit <= 0 || startTime.after(endTime)
                     || (type != ShopOrderRankType.ORDER_NUM_COUNT && type != ShopOrderRankType.ORDER_PRICE_COUNT)) {
                 return CommonResult.fail(HttpStatus.PARAMETER_ERROR);
@@ -72,7 +77,7 @@ public class ShopOrderRecordController {
             });
             return CommonResult.success(resultList);
         } catch (ParseException e) {
-            LOGGER.error("parse date error: [{}]", e);
+            LOGGER.error("解析时间异常: [{}]", e);
             return CommonResult.fail(HttpStatus.PARAMETER_ERROR);
         }
     }
