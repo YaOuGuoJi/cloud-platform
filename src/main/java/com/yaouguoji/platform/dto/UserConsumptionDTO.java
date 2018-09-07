e5c8d56388d458486f43664f4c87b89a7a09285e
lijingwei007 <42726605+lijingwei007@users.noreply.github.com>
2018-09-07 16:41:37 +0800

    save code

--- src/main/java/com/yaouguoji/platform/dto/UserConsumptionDTO.java
+++ src/main/java/com/yaouguoji/platform/dto/UserConsumptionDTO.java
@@ -3,7 +3,13 @@ package com.yaouguoji.platform.dto;
 import java.io.Serializable;
 
 public class UserConsumptionDTO implements Serializable {
-    private int userId;
+
+
+    //线上消费 线下消费，那个商店消费，月份消费总计，年份消费总计
+    /**
+     * 线上消费
+     */
+    private double onlineCSP;
 
 
 }
