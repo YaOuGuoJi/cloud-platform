5c4cca6c75d022ef03bd4398c1cc6fb4cf0496f6
lijingwei007 <1007843314@qq.com>
2018-09-17 16:12:41 +0800

    无

---
+++ src/main/java/com/yaouguoji/platform/impl/UserConsumptionServiceImpl.java
@@ -0,0 +1,60 @@
+package com.yaouguoji.platform.impl;
+
+import com.yaouguoji.platform.dto.OrderRecordDTO;
+import com.yaouguoji.platform.dto.UserConsumptionDTO;
+import com.yaouguoji.platform.entity.OrderRecordEntity;
+import com.yaouguoji.platform.entity.UserConsumptionEntity;
+import com.yaouguoji.platform.mapper.UserConsumptionMapper;
+import com.yaouguoji.platform.mapper.UserConsumptionOrderMapper;
+import com.yaouguoji.platform.service.UserConsumptionService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+@Service
+public class UserConsumptionServiceImpl implements UserConsumptionService {
+    @Resource
+    private UserConsumptionMapper userConsumptionMapper;
+    @Resource
+    private UserConsumptionOrderMapper userConsumptionOrderMapper;
+    @Override
+    public UserConsumptionDTO onlineCSP(int userId){
+        UserConsumptionEntity userConsumptionEntity = userConsumptionMapper.queryOnlineCSP(userId);
+        if(userConsumptionEntity == null){
+            return null;
+        }
+        UserConsumptionDTO userConsumptionDTO = new UserConsumptionDTO();
+        BeanUtils.copyProperties(userConsumptionEntity,userConsumptionDTO);
+        return userConsumptionDTO;
+    }
+    @Override
+    public UserConsumptionDTO shopCSP(int userId){
+        UserConsumptionEntity userConsumptionEntity = userConsumptionMapper.queryShopCSP(userId);
+        if(userConsumptionEntity == null){
+            return null;
+        }
+        UserConsumptionDTO userConsumptionDTO = new UserConsumptionDTO();
+        BeanUtils.copyProperties(userConsumptionEntity,userConsumptionDTO);
+        return userConsumptionDTO;
+    }
+
+    @Override
+    public int recentYearConsumption(int userId){
+
+        return userConsumptionMapper.recentYearCSP(userId);
+    }
+    @Override
+    public OrderRecordDTO getUserConsumptionOrders(int userId,Date startDate,Date endDate){
+        OrderRecordEntity orderRecordEntity = userConsumptionOrderMapper.getUserConsumptionOrders(userId,startDate,endDate);
+        if (orderRecordEntity == null) {
+            return null;
+        }
+        OrderRecordDTO orderRecordDTO = new OrderRecordDTO();
+        BeanUtils.copyProperties(orderRecordEntity,orderRecordDTO);
+        return orderRecordDTO;
+    }
+
+
+}
