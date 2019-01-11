c8a220ccc2a88331f0e9d40447b2e15dbda135fb
zh <277708272@qq.com>
2019-01-11 10:34:57 +0800

    添加小镇订单接口

---
+++ src/main/java/com/yaouguoji/platform/controller/TownOrderCountController.java
@@ -0,0 +1,14 @@
+package com.yaouguoji.platform.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangqiang
+ * @date 2019-01-11
+ */
+@RestController
+public class TownOrderCountController {
+
+
+
+}
