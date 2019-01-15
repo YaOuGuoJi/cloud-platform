d753c1b233b26ba49856f6a161a42c0dbe77e1a6
Liang <1144351205@qq.com>
2019-01-15 13:00:15 +0800

    liang

--- src/main/java/com/yaouguoji/platform/controller/TownOrderCountController.java
+++ src/main/java/com/yaouguoji/platform/controller/TownOrderCountController.java
@@ -2,6 +2,8 @@ package com.yaouguoji.platform.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.google.common.collect.Maps;
+import com.xianbester.api.dto.OrderRecordCountDTO;
+import com.xianbester.api.dto.OrderRecordRequest;
 import com.xianbester.api.service.OrderRecordService;
 import com.yaouguoji.platform.common.CommonResult;
 import com.yaouguoji.platform.common.CommonResultBuilder;
@@ -9,11 +11,11 @@ import com.yaouguoji.platform.enums.HttpStatus;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -62,8 +64,8 @@ public class TownOrderCountController {
             if (endTime.after(new Date())) {
                 return CommonResult.fail(HttpStatus.PARAMETER_ERROR.value, "结束时间必须小于当前时间");
             }
-            Map<String, BigDecimal> todayPriceAndFrequency = orderRecordService.townOrderRecordCount(startTime, endTime);
-            if (CollectionUtils.isEmpty(todayPriceAndFrequency)) {
+            OrderRecordCountDTO todayPriceAndFrequency = orderRecordService.townOrderRecordCount(createOrderRecordRequest(startTime, endTime));
+            if (todayPriceAndFrequency == null) {
                 return new CommonResultBuilder().code(200)
                         .message("无数据")
                         .data("price", 0)
@@ -84,17 +86,16 @@ public class TownOrderCountController {
         Date before24h = new DateTime(now).minusHours(24).toDate();
         Date before48h = new DateTime(now).minusHours(48).toDate();
         Date before7Days = new DateTime(now).minusDays(7).toDate();
-        Date before30Days = new DateTime(now).minusDays(30).toDate();
-        Map<String, BigDecimal> oneDayData = orderRecordService.townOrderRecordCount(before24h, now);
-        Map<String, BigDecimal> twoDayData = orderRecordService.townOrderRecordCount(before48h, before24h);
-        Map<String, BigDecimal> sevenDayData = orderRecordService.townOrderRecordCount(before7Days, now);
-        Map<String, BigDecimal> oneMonthData = orderRecordService.townOrderRecordCount(before30Days, now);
+        Date beforeOneMonth = new DateTime(now).minusMonths(1).toDate();
+        OrderRecordCountDTO oneDayData = orderRecordService.townOrderRecordCount(createOrderRecordRequest(before24h, now));
+        OrderRecordCountDTO twoDayData = orderRecordService.townOrderRecordCount(createOrderRecordRequest(before48h, before24h));
+        OrderRecordCountDTO sevenDayData = orderRecordService.townOrderRecordCount(createOrderRecordRequest(before7Days, now));
+        OrderRecordCountDTO oneMonthData = orderRecordService.townOrderRecordCount(createOrderRecordRequest(beforeOneMonth, now));
         Map<String, Map<String, BigDecimal>> orderCount = Maps.newHashMap();
-
-        orderCount.put("todayOrder", createResult(twoDayData, oneDayData, "frequency"));
-        orderCount.put("todayPeople", createResult(twoDayData, oneDayData, "peopleNum"));
-        orderCount.put("todayPrice", createResult(twoDayData, oneDayData, "price"));
-        orderCount.put("todayCustomerPrice", createResult(createMedian(twoDayData), createMedian(oneDayData), "customerPrice"));
+        orderCount.put("frequency", createResult(twoDayData, oneDayData, "frequency"));
+        orderCount.put("price", createResult(twoDayData, oneDayData, "price"));
+        orderCount.put("peopleNum", createResult(twoDayData, oneDayData, "peopleNum"));
+        orderCount.put("averagePrice", createResult(twoDayData, oneDayData, "averagePrice"));
         orderCount.put("sevenDayTotal", createTotal(sevenDayData));
         orderCount.put("oneMonthTotal", createTotal(oneMonthData));
         return CommonResult.success(orderCount);
@@ -113,58 +114,79 @@ public class TownOrderCountController {
     /**
      * 计算增长，生成新的map集合
      *
-     * @param beforeMap 时间较早的集合
-     * @param afterMap  时间较晚的集合
-     * @param key       处理的数据键值
+     * @param beforeData
+     * @param afterData
+     * @param field
      * @return
      */
-    private Map<String, BigDecimal> createResult(Map<String, BigDecimal> beforeMap, Map<String, BigDecimal> afterMap, String key) {
+    Map<String, BigDecimal> createResult(OrderRecordCountDTO beforeData, OrderRecordCountDTO afterData, String field) {
+        Class dataClass = beforeData.getClass();
+        Field fieldClass;
         HashMap<String, BigDecimal> resultMap = Maps.newHashMap();
-        resultMap.put(key, afterMap.get(key));
-        if (beforeMap.get(key) == null || beforeMap.get(key).equals(BigDecimal.valueOf(0))) {
-            if (beforeMap.get(key) == null || beforeMap.get(key).equals(BigDecimal.valueOf(0))) {
-                resultMap.put(key, BigDecimal.valueOf(0));
-                resultMap.put("raise", BigDecimal.valueOf(0));
+        try {
+            fieldClass = dataClass.getDeclaredField(field);
+            fieldClass.setAccessible(true);
+            BigDecimal beforeValue = (BigDecimal) fieldClass.get(beforeData);
+            BigDecimal afterValue = (BigDecimal) fieldClass.get(afterData);
+            if (afterData == null) {
+                resultMap.put(field, BigDecimal.ZERO);
             } else {
-                resultMap.put("raise", BigDecimal.valueOf(5));
+                resultMap.put(field, afterValue);
             }
-        } else {
-            resultMap.put("raise", afterMap.get(key).divide(beforeMap.get(key), 2, BigDecimal.ROUND_HALF_UP).subtract(BigDecimal.valueOf(1)));
+            if (beforeData.equals(BigDecimal.ZERO)) {
+                if (afterData.equals(BigDecimal.ZERO)) {
+                    resultMap.put("raise", BigDecimal.ZERO);
+                }
+                resultMap.put("raise", BigDecimal.valueOf(5));
+                return resultMap;
+            }
+            resultMap.put("raise", afterValue.divide(beforeValue, 2, BigDecimal.ROUND_HALF_UP).subtract(BigDecimal.valueOf(1)));
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            LOGGER.error("反射获取字段类错误");
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            LOGGER.error("反射获取字段值错误");
         }
-        return resultMap;
-    }
-
-    /**
-     * 计算客单价（消费额/人数），生成map集合
-     *
-     * @param startMap
-     * @return
-     */
-    private Map<String, BigDecimal> createMedian(Map<String, BigDecimal> startMap) {
-        HashMap<String, BigDecimal> resultMap = Maps.newHashMap();
-        if (startMap.get("peopleNum").equals(BigDecimal.valueOf(0)) || startMap.get("peopleNum") == null) {
-            resultMap.put("customerPrice", BigDecimal.valueOf(0));
-            return resultMap;
-        }
-        resultMap.put("customerPrice", startMap.get("price").divide(startMap.get("peopleNum"), 10, BigDecimal.ROUND_HALF_UP));
         return resultMap;
     }
 
     /**
      * 生成概况
      *
-     * @param startMap
+     * @param orderRecordCountDTO
      * @return
      */
-    private Map<String, BigDecimal> createTotal(Map<String, BigDecimal> startMap) {
+    private Map<String, BigDecimal> createTotal(OrderRecordCountDTO orderRecordCountDTO) {
         HashMap<String, BigDecimal> resultMap = Maps.newHashMap();
-        if (startMap.get("price") == null || startMap.get("price").equals(BigDecimal.valueOf(0))) {
-            resultMap.put("price", BigDecimal.valueOf(0));
+        if (orderRecordCountDTO.getPrice() == null) {
+            resultMap.put("price", BigDecimal.ZERO);
         } else {
-            resultMap.put("price", startMap.get("price"));
+            resultMap.put("price", orderRecordCountDTO.getPrice());
         }
-        resultMap.put("frequency", startMap.get("frequency"));
-        resultMap.put("peopleNum", startMap.get("peopleNum"));
+        if (orderRecordCountDTO.getFrequency() == null) {
+            resultMap.put("frequency", BigDecimal.ZERO);
+        } else {
+            resultMap.put("frequency", orderRecordCountDTO.getFrequency());
+        }
+        if (orderRecordCountDTO.getPeopleNum() == null) {
+            resultMap.put("peopleNum", BigDecimal.ZERO);
+        }
+        resultMap.put("peopleNum", orderRecordCountDTO.getPeopleNum());
         return resultMap;
+    }
+
+    /**
+     * 创建service层传入参数
+     *
+     * @param start
+     * @param end
+     * @return
+     */
+    private OrderRecordRequest createOrderRecordRequest(Date start, Date end) {
+        OrderRecordRequest orderRecordRequest = new OrderRecordRequest();
+        orderRecordRequest.setStartTime(start);
+        orderRecordRequest.setEndTime(end);
+        return orderRecordRequest;
     }
 }
