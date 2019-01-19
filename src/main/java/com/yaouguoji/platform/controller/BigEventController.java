a2b864348813742568c9a9d172c9c9728223a2fb
zh <277708272@qq.com>
2019-01-19 18:55:47 +0800

    添加接口--活动部分：查询一月内参与活动人数

--- src/main/java/com/yaouguoji/platform/controller/BigEventController.java
+++ src/main/java/com/yaouguoji/platform/controller/BigEventController.java
@@ -8,6 +8,7 @@ import com.xianbester.api.service.CameraRecordService;
 import com.yaouguoji.platform.common.CommonResult;
 import com.yaouguoji.platform.enums.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -28,27 +29,53 @@ public class BigEventController {
     @Reference
     private CameraRecordService cameraRecordService;
 
+    @ModelAttribute
+    public BigEventDTO get(Integer eventId) {
+        if (eventId <= 0) {
+            return null;
+        }
+        BigEventDTO bigEvent = bigEventService.getBigEvent(eventId);
+        if (bigEvent == null) {
+            return null;
+        }
+        return bigEvent;
+    }
+
     /**
      * 查询今日活动参加人数
      *
-     * @param eventId
+     * @param bigEventDTO
      * @return
      */
     @GetMapping("/event/todayNumberOfPeople")
-    public CommonResult numberOfParticipantsInTodayIsEvent(Integer eventId) {
-        if (eventId < 0) {
-            return CommonResult.fail(HttpStatus.PARAMETER_ERROR);
-        }
-        BigEventDTO bigEventEntity = bigEventService.getBigEventEntity(eventId);
-        if (bigEventEntity == null) {
+    public CommonResult numberOfParticipantsInTodayIsEvent(BigEventDTO bigEventDTO) {
+        if (bigEventDTO == null) {
             return CommonResult.fail(HttpStatus.NOT_FOUND);
         }
-        Integer areaId = bigEventEntity.getAreaId();
+        Integer areaId = bigEventDTO.getAreaId();
         Integer result = cameraRecordService.queryParticipantByTime(areaId, TODAY);
         Map<String, Integer> map = Maps.newHashMap();
         map.put("todayEventNum", result == null ? 0 : result);
         return CommonResult.success(map);
     }
 
+    /**
+     * 查询一月内参与活动人数(线上，线下)
+     *
+     * @param bigEventDTO
+     * @return
+     */
+    @GetMapping("/event/monthNumberOfEvent")
+    public CommonResult numberOfParticipantsInMonthIsEvent(BigEventDTO bigEventDTO) {
+        if (bigEventDTO == null) {
+            return CommonResult.fail(HttpStatus.NOT_FOUND);
+        }
+        Integer offlinePeopleNum = bigEventService.getOfflinePeopleNum(bigEventDTO.getId());
+        Integer onlineResult = bigEventDTO.getEventApplyNum();
+        Map<String, Integer> map = Maps.newHashMap();
+        map.put("online", onlineResult == null ? 0 : onlineResult);
+        map.put("offline", offlinePeopleNum == null ? 0 : offlinePeopleNum);
+        return CommonResult.success(map);
+    }
 
 }
