8856663ca1adb764211cebab01f92c08dc514719
Hut <42726605+lijingwei007@users.noreply.github.com>
2019-01-23 15:49:04 +0800

    update

---
+++ src/main/java/com/yaouguoji/platform/controller/OrderVolumeController.java
@@ -0,0 +1,14 @@
+package com.yaouguoji.platform.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author lizhi
+ * @date 2019-01-23
+ */
+@RestController
+public class OrderVolumeController {
+    private
+}
